39e0613ade042722baadc48aa96ce4aeea9b5676
rodrisout <rodrisout@gmail.com>
2025-05-07 18:54:12 +0200

    api

---
+++ vithabitus-api/src/test/java/com/vithabitus/api/VithabitusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vithabitus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VithabitusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
